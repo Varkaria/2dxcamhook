d99dcd24fe926329fa4742f0d240a6076653ef30
varkaria <61514399+varkaria@users.noreply.github.com>
2024-04-03 23:55:03 +0700

    feat: add 2024032500 offsets

---
+++ src/game/LDJ-010-2024032500.cc
@@ -0,0 +1,6 @@
+#include <cstddef>
+
+std::ptrdiff_t addr_hook_a        = 0x087FF70;
+std::ptrdiff_t addr_hook_b        = 0x08EA320;
+std::ptrdiff_t addr_textures      = 0x784F028;
+std::ptrdiff_t addr_device_offset = 0x00000E0;
